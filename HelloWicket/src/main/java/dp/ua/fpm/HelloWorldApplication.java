1483b111d1b20e137898202a9904b9a3fb1f34c0
(no author) <(no author)@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-05-03 20:33:06 +0000

    +Sprint backlog +Исправления hello portlet
    git-svn-id: svn://109.206.40.61@5 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- HelloWicket/src/main/java/dp/ua/fpm/HelloWorldApplication.java
+++ HelloWicket/src/main/java/dp/ua/fpm/HelloWorldApplication.java
@@ -1,15 +1,13 @@
 package dp.ua.fpm;
 
-import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
 public class HelloWorldApplication extends WebApplication {
 	public HelloWorldApplication() {
 		
 	}
-
-        @Override
-	public Class<? extends WebPage> getHomePage() {
+	
+	public Class getHomePage() {
 		return GuestBook.class;
 	}
 
