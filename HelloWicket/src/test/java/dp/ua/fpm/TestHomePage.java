3c4672de1a7bf249af24f41b1ae7d4e87f2df1dc
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-05-06 20:17:57 +0000

    +test
    git-svn-id: svn://109.206.40.61@9 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- HelloWicket/src/test/java/dp/ua/fpm/TestHomePage.java
+++ HelloWicket/src/test/java/dp/ua/fpm/TestHomePage.java
@@ -19,10 +19,10 @@ public class TestHomePage extends TestCase
 	public void testRenderMyPage()
 	{
 		//start and render the test page
-		//tester.startPage(HelloWorld.class);
+		tester.startPage(GuestBook.class);
 
 		//assert rendered page class
-		//tester.assertRenderedPage(HelloWorld.class);
+                tester.assertRenderedPage(GuestBook.class);
 
 		//assert rendered label component
 		//tester.assertLabel("message", "If you see this message wicket is properly configured and running");
