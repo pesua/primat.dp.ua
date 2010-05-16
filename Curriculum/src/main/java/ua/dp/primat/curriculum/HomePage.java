38c3d23e38abd714b27329aa5a9dc63653be0645
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-05-16 22:36:24 +0000

    +face
    git-svn-id: svn://109.206.40.61@61 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- Curriculum/src/main/java/ua/dp/primat/curriculum/HomePage.java
+++ Curriculum/src/main/java/ua/dp/primat/curriculum/HomePage.java
@@ -1,18 +1,22 @@
 package ua.dp.primat.curriculum;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.PropertyModel;
 import ua.dp.primat.curriculum.data.StudentGroup;
+import ua.dp.primat.curriculum.data.Workload;
+import ua.dp.primat.curriculum.data.WorkloadEntry;
 
 /**
  * Homepage
@@ -30,24 +34,29 @@ public class HomePage extends WebPage {
 	 *            Page parameters
 	 */
     StudentGroup choosenGroup;
-    
+    Long choosenSemester;
+    ListView<WorkloadEntry> disciplinesView;
     
     public HomePage(final PageParameters parameters) {
+        //load data from database
         EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("curriculum");
         EntityManager em = emFactory.createEntityManager();
-        //logic
+       
         em.getTransaction().begin();
 
         final List groups = em.createQuery("from StudentGroup").getResultList();
+        
+        em.close();
+
         if(choosenGroup == null)
             choosenGroup = (StudentGroup) groups.get(0);
 
-        em.close();
+        if(choosenSemester == null)
+            choosenSemester = new Long(1);
+
         Form form = new Form("form");
         add(form);
-
-
-        DropDownChoice ddc = new DropDownChoice("group",
+        DropDownChoice groupChoise = new DropDownChoice("group",
                 new PropertyModel(this, "choosenGroup"),
                 new LoadableDetachableModel() {
 
@@ -56,6 +65,46 @@ public class HomePage extends WebPage {
                 return groups;
             }
         });
-        form.add(ddc);
+        form.add(groupChoise);
+
+        DropDownChoice semesterChoise = new DropDownChoice("semester",
+                new PropertyModel(this, "choosenSemester"),
+                new LoadableDetachableModel() {
+
+            @Override
+            protected Object load() {
+                List l = new ArrayList();
+                for (int i = 1; i <= 8; i++)
+                    l.add(new Long(i));
+                return l;
+            }
+        });
+        form.add(semesterChoise);
+
+        //TODO must be refactored
+        List<Workload> workloads = choosenGroup.getWorkloads();
+        List<WorkloadEntry> workloadEntries = new ArrayList();
+        for(Workload w: workloads) {
+            for(WorkloadEntry we: w.getEntries()) {
+                if(we.getSemesterNumber() == choosenSemester)
+                    workloadEntries.add(we);
+            }
+        }
+        
+        add(disciplinesView = new ListView<WorkloadEntry>("disciplineRow", workloadEntries) {
+
+            @Override
+            protected void populateItem(ListItem<WorkloadEntry> li) {
+                final WorkloadEntry entry = li.getModelObject();
+                li.add(new Label("disciplineName", entry.getWorkload().getDiscipline().getName()));
+                li.add(new Label("cathedra", entry.getWorkload().getDiscipline().getCathedra().getName()));
+                li.add(new Label("lection", entry.getLectionCount().toString()));
+                li.add(new Label("labs", entry.getLabCount().toString()));
+                li.add(new Label("selfwork", entry.getIndCount().toString()));
+                li.add(new Label("course", entry.getCourceWork().toString()));
+                li.add(new Label("finalcontrol", entry.getFinalControl().toString()));
+            }
+        });
+
     }
 }
