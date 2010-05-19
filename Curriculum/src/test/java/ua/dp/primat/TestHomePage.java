49d26ffcd8125d4bbc5a8dc535bdb3e6ad5233eb
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-05-19 10:57:32 +0000

    +test
    git-svn-id: svn://109.206.40.61@74 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- Curriculum/src/test/java/ua/dp/primat/TestHomePage.java
+++ Curriculum/src/test/java/ua/dp/primat/TestHomePage.java
@@ -1,29 +1,40 @@
 package ua.dp.primat;
 
-import junit.framework.TestCase;
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import ua.dp.primat.curriculum.HomePage;
 import ua.dp.primat.curriculum.WicketApplication;
 
 /**
  * Simple test using the WicketTester
  */
-public class TestHomePage extends TestCase
-{
+public class TestHomePage {
 	private WicketTester tester;
 
-	@Override
+        /*
+         * Basic set up of the test
+         */
+	@Before
 	public void setUp()
 	{
             tester = new WicketTester(new WicketApplication());
-	}
 
-	public void testRenderMyPage()
-	{
             //start and render the test page
             tester.startPage(HomePage.class);
+	}
 
+        @Test
+	public void testRenderMyPage()
+	{
             //assert rendered page class
             tester.assertRenderedPage(HomePage.class);
+
+            System.out.println(tester.getApplication().getHomePage());
+
+            DropDownChoice dChoice = (DropDownChoice) tester.getComponentFromLastRenderedPage("form:semester");
+            Assert.assertTrue(dChoice.getChoices().size() == 8);
 	}
 }
