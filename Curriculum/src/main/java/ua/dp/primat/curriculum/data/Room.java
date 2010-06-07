01c747e707ba20a588173493b568124b020cd92a
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-06-07 20:00:08 +0000

    4.1 add gettors and settors
    git-svn-id: svn://109.206.40.61@153 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- Curriculum/src/main/java/ua/dp/primat/curriculum/data/Room.java
+++ Curriculum/src/main/java/ua/dp/primat/curriculum/data/Room.java
@@ -26,4 +26,28 @@ public class Room implements Serializable {
         this.building = building;
         this.number = number;
     }
+
+    public Long getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(Long building) {
+        this.building = building;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
 }
