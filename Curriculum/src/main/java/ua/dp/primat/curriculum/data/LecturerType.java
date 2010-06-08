c6a51370a8a44ca177fe4a2b2c5b1c74043c5a9b
pes_ua <pes_ua@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-06-08 11:46:21 +0000

    git-svn-id: svn://109.206.40.61@164 62292e2b-fded-4b55-83dd-b5fccdba3bda

---
+++ Curriculum/src/main/java/ua/dp/primat/curriculum/data/LecturerType.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ua.dp.primat.curriculum.data;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author pesua
+ */
+public enum LecturerType {
+    ASSIATANT {
+
+        @Override
+        public String toString() {
+            return BUNDLE.getString("lecturertype.assistant");
+        }
+
+    },
+    SENIORLECTURER {
+
+        @Override
+        public String toString() {
+            return BUNDLE.getString("lecturertype.seniorlecturer");
+        }
+
+    },
+    DOCENT {
+
+        @Override
+        public String toString() {
+            return BUNDLE.getString("lecturertype.docent");
+        }
+    },
+    PROFESSOR {
+
+        @Override
+        public String toString() {
+            return BUNDLE.getString("lecturertype.professor");
+        }
+    };
+
+private static final ResourceBundle BUNDLE = ResourceBundle.getBundle("dimainModel", new Locale("uk"));
+}
