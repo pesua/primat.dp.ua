f08aaa7389bb3ebdb1395190bca2976f797695ae
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-06-23 11:41:12 +0000

    11.2 ajax
    git-svn-id: svn://109.206.40.61@236 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- Curriculum/src/main/java/ua/dp/primat/schedule/admin/EditableScheduleItemPanel.java
+++ Curriculum/src/main/java/ua/dp/primat/schedule/admin/EditableScheduleItemPanel.java
@@ -11,21 +11,32 @@ import ua.dp.primat.schedule.services.LessonItem;
  * @author EniSh
  */
 public final class EditableScheduleItemPanel extends Panel {
-    public EditableScheduleItemPanel(String id, LessonItem li) {
+    public EditableScheduleItemPanel(String id, final LessonItem li) {
         super (id);
-//        add(new AjaxCheckBox("singleLesson", new Model<Boolean>(lessonType)) {
-//
-//            @Override
-//            protected void onUpdate(AjaxRequestTarget art) {
-//                //TODO
-//                throw new UnsupportedOperationException("Not supported yet.");
-//            }
-//        });
 
         add(new EditableLesonPanel("numerator", li.getNumerator()));
-        EditableLesonPanel elp = new EditableLesonPanel("denominator", li.getDenominator());
+        final EditableLesonPanel elp = new EditableLesonPanel("denominator", li.getDenominator());
         elp.setVisible(!li.isOneLesson());
+        elp.setOutputMarkupId(true);
+        elp.setOutputMarkupPlaceholderTag(true);
         add(elp);
+
+        
+        add(new AjaxCheckBox("singleLesson", new Model<Boolean>(lessonType)) {
+
+            @Override
+            protected void onUpdate(AjaxRequestTarget target) {
+                lessonType = !lessonType;
+                elp.setVisible(lessonType);
+                if (lessonType) {
+                    li.setOneLesson();
+                } else {
+                    li.setTwoLesson();
+                }
+                target.addComponent(elp);
+                target.addComponent(elp.getParent());
+            }
+        });
     }
 
     private boolean lessonType;
