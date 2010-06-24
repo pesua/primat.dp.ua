befb080b9e62fd9c87c710681f4cf8b2ed5d76ed
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-06-24 20:08:39 +0000

    11.2
    git-svn-id: svn://109.206.40.61@275 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- Curriculum/src/main/java/ua/dp/primat/schedule/admin/EditableLesonPanel.java
+++ Curriculum/src/main/java/ua/dp/primat/schedule/admin/EditableLesonPanel.java
@@ -1,13 +1,16 @@
 package ua.dp.primat.schedule.admin;
 
+import java.util.Arrays;
 import java.util.List;
 import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import ua.dp.primat.domain.workload.Discipline;
 import ua.dp.primat.domain.Lecturer;
 import ua.dp.primat.domain.Room;
+import ua.dp.primat.domain.lesson.LessonType;
 import ua.dp.primat.schedule.services.EditScheduleService;
 import ua.dp.primat.schedule.services.EditableLesson;
 
@@ -23,11 +26,15 @@ private static final long serialVersionUID = 1L;
         final List<Room> rooms = editScheduleService.getRooms();
         final List<Discipline> disciplines = editScheduleService.getDisciplines();
         final List<Lecturer> lecturers = editScheduleService.getLecturers();
+        final List<LessonType> types = Arrays.asList(LessonType.values());
+
+        add(new FeedbackPanel("feedback"));
 
         add(new DropDownChoice<Discipline>("disciplineDDC", new PropertyModel<Discipline>(lesson, "discipline"), disciplines));
         add(new DropDownChoice<Room>("roomDDC", new PropertyModel<Room>(lesson, "room"), rooms));
         add(new DropDownChoice<Lecturer>("lecturerDDC", new PropertyModel<Lecturer>(lesson, "lecturer"), lecturers));
         add(new DropDownChoice<Lecturer>("asistantDDC", new PropertyModel<Lecturer>(lesson, "asistant"), lecturers));
+        add(new DropDownChoice<LessonType>("typeDDC", new PropertyModel<LessonType>(lesson, "lessonType"), types));
     }
 
     @SpringBean
