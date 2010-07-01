15c6120f16565d244f248e0fb33fc0cecfa28a9b
fdevelop <fdevelop@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-07-01 13:24:15 +0000

    5.4 wicket markup first steps
    git-svn-id: svn://109.206.40.61@375 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- portlets/userinform/src/main/java/ua/dp/primat/portlet/userinform/app/UserInfoPanel.java
+++ portlets/userinform/src/main/java/ua/dp/primat/portlet/userinform/app/UserInfoPanel.java
@@ -1,6 +1,10 @@
 package ua.dp.primat.portlet.userinform.app;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.model.User;
+import com.liferay.portal.model.UserGroup;
+import com.liferay.portal.service.UserServiceUtil;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 
@@ -12,7 +16,34 @@ public class UserInfoPanel extends Panel {
 
     public UserInfoPanel(String id, User user) {
         super(id);
-        add(new Label("fullName", user.getFullName()));
+        add(new Label("fullname", user.getFullName()));
+        add(new Label("email", user.getDisplayEmailAddress()));
+        add(new Label("screenname", user.getScreenName()));
+        add(new Label("birthday", getUserBirthday(user)));
+        add(new Label("group", getUserGroups(user)));
+    }
+
+    private String getUserGroups(User user) {
+        try {
+            StringBuilder groups = new StringBuilder();
+            for (UserGroup ug : user.getUserGroups()) {
+                groups.append(ug.getName());
+                groups.append(" | ");
+            }
+            return groups.toString();
+        } catch (SystemException se) {
+            return "-";
+        }
+    }
+
+    private String getUserBirthday(User user) {
+        try {
+            return user.getBirthday().toString();
+        } catch (PortalException pe) {
+            return "-";
+        } catch (SystemException se) {
+            return "-";
+        }
     }
 
     private static final long serialVersionUID = 1L;
