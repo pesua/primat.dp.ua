62e4028c566ec196eafbbd86fa157d4f66592f26
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-07-15 15:51:36 +0000

    +BigBoard
    git-svn-id: svn://109.206.40.61@422 62292e2b-fded-4b55-83dd-b5fccdba3bda

---
+++ BigBoard/src/main/java/ua/dp/primat/bigboard/HomePage.java
@@ -0,0 +1,12 @@
+package ua.dp.primat.bigboard;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.image.Image;
+
+public class HomePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+    public HomePage(final PageParameters parameters) {
+    }
+}
