cd971d086e6964a87dda62165a04d0b01a239388
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-07-21 05:36:19 +0000

    git-svn-id: svn://109.206.40.61@469 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- Curriculum/src/main/webapp/js/print.js
+++ Curriculum/src/main/webapp/js/print.js
@@ -2,7 +2,7 @@ function showForPrint(pr) {
     newWin=window.open('','printWindow','width=800,height=600');
     newWin.document.open();
 
-    newWin.document.write("<html><head><title>On-the-fly");
+    newWin.document.write("<html><head><title>Version for print");
     newWin.document.write("</title></head><body>");
     newWin.document.write("<a href='javascript:window.print();'>Print</a>");
     newWin.document.write(pr);
