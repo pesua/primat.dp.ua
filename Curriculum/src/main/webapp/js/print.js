d0c2ad005c3a42bd2b6bab85e51b3cbacac6b31f
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-07-22 11:33:06 +0000

    36
    git-svn-id: svn://109.206.40.61@477 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- Curriculum/src/main/webapp/js/print.js
+++ Curriculum/src/main/webapp/js/print.js
@@ -1,10 +1,12 @@
-function showForPrint(pr) {
+function showForPrint(pr, cssUrl) {
     newWin=window.open('','printWindow','width=800,height=600');
     newWin.document.open();
 
     newWin.document.write("<html><head><title>Version for print");
-    newWin.document.write("</title></head><body>");
-    newWin.document.write("<a href='javascript:window.print();'>Print</a>");
+    newWin.document.write("</title>");
+    newWin.document.write("<link rel='stylesheet' type='text/css' href='/Curriculum/print.css' />");
+    newWin.document.write("</head><body>");
+    newWin.document.write("<a href='javascript:window.print();'><img src='http://www.iconsearch.ru/uploads/icons/gnomeicontheme/24x24/stock_print.png' /></a>");
     newWin.document.write(pr);
     newWin.document.write("</body></html>");
 
