bc07b598001c2493836682809a6ded92d4a125d5
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-07-28 10:24:15 +0000

    15
    git-svn-id: svn://109.206.40.61@555 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- Curriculum/src/main/java/ua/dp/primat/schedule/widget/ViewPage.java
+++ Curriculum/src/main/java/ua/dp/primat/schedule/widget/ViewPage.java
@@ -1,19 +1,44 @@
 package ua.dp.primat.schedule.widget;
 
+import java.util.List;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import ua.dp.primat.domain.StudentGroup;
+import ua.dp.primat.domain.lesson.DayOfWeek;
+import ua.dp.primat.domain.lesson.Lesson;
+import ua.dp.primat.domain.lesson.WeekType;
+import ua.dp.primat.repositories.StudentGroupRepository;
+import ua.dp.primat.schedule.services.LessonService;
+import ua.dp.primat.schedule.view.daybook.DayPanel;
 
 /**
  *
  * @author EniSh
  */
 public final class ViewPage extends WebPage {
+
     public ViewPage() {
-        super ();
+        super();
+        StudentGroup group = groupRepository.getGroups().get(0);
+        List<Lesson> lessons = lessonService.getLessonsForGroupAndSemester(group, 4L);
+        today = new DayPanel("today", DayOfWeek.MONDAY);
+        today.updateInfo(lessonService.getLessonsPerDay(lessons, DayOfWeek.MONDAY, WeekType.NUMERATOR));
+        add(today);
+        
+        tomorrow = new DayPanel("tomorrow", DayOfWeek.TUESDAY);
+        tomorrow.updateInfo(lessonService.getLessonsPerDay(lessons, DayOfWeek.TUESDAY, WeekType.NUMERATOR));
+        add(tomorrow);
     }
 
     public ViewPage(PageParameters params) {
-        //TODO:  process page parameters
+        this();
     }
-}
 
+    private DayPanel today;
+    private DayPanel tomorrow;
+    @SpringBean
+    private LessonService lessonService;
+    @SpringBean
+    private StudentGroupRepository groupRepository;
+}
