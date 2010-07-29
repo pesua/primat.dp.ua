71d6a171dfb4cd56ba3e2fe8554acef4888e7576
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-07-29 13:34:35 +0000

    15 -violations
    git-svn-id: svn://109.206.40.61@574 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- Curriculum/src/main/java/ua/dp/primat/schedule/widget/NoSchedulePage.java
+++ Curriculum/src/main/java/ua/dp/primat/schedule/widget/NoSchedulePage.java
@@ -1,6 +1,5 @@
 package ua.dp.primat.schedule.widget;
 
-import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
 
 /**
@@ -9,11 +8,8 @@ import org.apache.wicket.markup.html.WebPage;
  */
 public final class NoSchedulePage extends WebPage {
     public NoSchedulePage() {
-        super ();
     }
-
-    public NoSchedulePage(PageParameters params) {
-        //TODO:  process page parameters
-    }
+    
+    private static final long serialVersionUID = 1L;
 }
 
