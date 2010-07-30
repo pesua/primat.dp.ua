0787e605e2aa072778457e24e0195752d9b0761a
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-07-30 05:27:25 +0000

    15
    git-svn-id: svn://109.206.40.61@593 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- Curriculum/src/main/java/ua/dp/primat/schedule/view/LecturerSchedulePage.java
+++ Curriculum/src/main/java/ua/dp/primat/schedule/view/LecturerSchedulePage.java
@@ -15,14 +15,15 @@ import ua.dp.primat.schedule.services.TimeService;
  * @author EniSh
  */
 public final class LecturerSchedulePage extends WebPage {
+
     public LecturerSchedulePage() {
-        super ();
+        super();
         List<Lecturer> lecturers = lecturerRepository.getAllLecturers();
         lecturer = lecturers.get(0);
-        
+
         add(new DropDownChoice<Lecturer>(
-                "lecturerChoise", 
-                new PropertyModel<Lecturer>(this, "lecturer"), 
+                "lecturerChoise",
+                new PropertyModel<Lecturer>(this, "lecturer"),
                 lecturers) {
 
             @Override
@@ -30,23 +31,23 @@ public final class LecturerSchedulePage extends WebPage {
                 super.onSelectionChanged(newSelection);
                 schedulePanel.refreshView(lessonService.getLessonsForLecturerBySemester(lecturer, timeService.currentSemester()));
             }
-            
+
+            @Override
+            protected boolean wantOnSelectionChangedNotifications() {
+                return true;
+            }
         });
-        
+
         schedulePanel = new ViewSchedulePanel("tabs");
         add(schedulePanel);
     }
-
     private ViewSchedulePanel schedulePanel;
     private static final long serialVersionUID = 1L;
     @SpringBean
     private LessonService lessonService;
     @SpringBean
     private TimeService timeService;
-
     private Lecturer lecturer;
-
     @SpringBean
     private LecturerRepository lecturerRepository;
 }
-
