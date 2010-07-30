a58ca609a5fc3fd2c064a1b0c78f9c093b44db6f
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-07-30 09:55:46 +0000

    15
    git-svn-id: svn://109.206.40.61@597 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- Curriculum/src/main/java/ua/dp/primat/schedule/view/RoomSchedulePage.java
+++ Curriculum/src/main/java/ua/dp/primat/schedule/view/RoomSchedulePage.java
@@ -1,6 +1,14 @@
 package ua.dp.primat.schedule.view;
 
+import java.util.List;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import ua.dp.primat.domain.Room;
+import ua.dp.primat.repositories.RoomRepository;
+import ua.dp.primat.schedule.services.LessonService;
+import ua.dp.primat.schedule.services.TimeService;
 
 /**
  *
@@ -8,7 +16,41 @@ import org.apache.wicket.markup.html.WebPage;
  */
 public final class RoomSchedulePage extends WebPage {
     public RoomSchedulePage() {
-        super ();
+        super ();final List<Room> lecturers = roomRepository.getRooms();
+        room = lecturers.get(0);
+
+        add(new DropDownChoice<Room>(
+                "roomChoise",
+                new PropertyModel<Room>(this, "room"),
+                lecturers) {
+
+            @Override
+            protected void onSelectionChanged(Room newSelection) {
+                super.onSelectionChanged(newSelection);
+                schedulePanel.refreshView(lessonService.getLessonForRoomBySemester(room, timeService.currentSemester()));
+            }
+
+            @Override
+            protected boolean wantOnSelectionChangedNotifications() {
+                return true;
+            }
+        });
+
+        schedulePanel = new ViewSchedulePanel("lessonView");
+        schedulePanel.setLecturerVisible(false);
+        add(schedulePanel);
+
+        schedulePanel.refreshView(lessonService.getLessonForRoomBySemester(room, timeService.currentSemester()));
     }
+    private ViewSchedulePanel schedulePanel;
+    @SpringBean
+    private LessonService lessonService;
+    @SpringBean
+    private TimeService timeService;
+    private Room room;
+    @SpringBean
+    private RoomRepository roomRepository;
+
+    private static final long serialVersionUID = 1L;
 }
 
