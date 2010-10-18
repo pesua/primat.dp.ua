a0f583d4de27ac614b83592b929db05e02c7ec36
enish <enish@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-10-18 04:45:39 +0000

    +banner
    git-svn-id: svn://109.206.40.61@711 62292e2b-fded-4b55-83dd-b5fccdba3bda

--- Liferay-ext/login-ext/docroot/WEB-INF/ext-impl/src/com/liferay/portlet/login/action/CreateAccountAction.java
+++ Liferay-ext/login-ext/docroot/WEB-INF/ext-impl/src/com/liferay/portlet/login/action/CreateAccountAction.java
@@ -248,7 +248,7 @@ public class CreateAccountAction extends PortletAction {
                 groupIds, organizationIds, roleIds, userGroupIds, sendEmail,
                 serviceContext);
                 
-        //user = UserLocalServiceUtil.updateActive(user.getUserId(), false);
+        user = UserLocalServiceUtil.updateActive(user.getUserId(), true);
 
         String from = "math.app.fpm@gmail.com";
         String to = "math.app.fpm@gmail.com";
