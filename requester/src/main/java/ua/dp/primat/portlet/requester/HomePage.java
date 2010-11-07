c3c96b7bda97b5b232ba0291da613ff4ae36cd32
pes_ua <pes_ua@62292e2b-fded-4b55-83dd-b5fccdba3bda>
2010-11-07 19:05:53 +0000

    MSDN AA request
    git-svn-id: svn://109.206.40.61@738 62292e2b-fded-4b55-83dd-b5fccdba3bda

---
+++ requester/src/main/java/ua/dp/primat/portlet/requester/HomePage.java
@@ -0,0 +1,73 @@
+package ua.dp.primat.portlet.requester;
+
+import com.liferay.util.mail.MailEngine;
+import com.liferay.util.mail.MailEngineException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.PropertyModel;
+
+/**
+ * Homepage
+ */
+public class HomePage extends WebPage {
+
+    private static final long serialVersionUID = 1L;
+    // TODO Add any page properties or variables here
+    /**
+     * Constructor that is invoked when page is invoked without a session.
+     *
+     * @param parameters
+     *            Page parameters
+     */
+    private final static List PRODUCTS = Arrays.asList(new String[]{
+        "красный", "желтый", "зеленый"});
+
+    public final class SelectionForm extends Form {
+
+        private String selectedProduct = "нет";
+
+        public String getSelectedProduct() {
+            return selectedProduct;
+        }
+
+        public void setSelectedProduct(String selectedColor) {
+            this.selectedProduct = selectedColor;
+        }
+
+        public SelectionForm(String s) {
+            super(s);
+            selectedProduct = (String)PRODUCTS.get(0);
+            //add(new Label("colorLabel", new PropertyModel(this, "selectedColor")));
+            add(new DropDownChoice("product", new PropertyModel(this, "selectedProduct"), PRODUCTS));
+
+            add(new Button("submitButton") {
+
+                @Override
+                public void onSubmit() {
+                    System.out.println("Selected product: " + getSelectedProduct());
+
+                    String from = "math.app.fpm@gmail.com";
+                    String to = "math.app.fpm@gmail.com";
+                    String subject = "MSDN AA request!";
+                    try {
+                        MailEngine.send(from, to, subject, "test-test");
+                    } catch (MailEngineException ex) {
+                        Logger.getLogger(HomePage.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+
+                }
+            });
+        }
+    }
+        public HomePage(final PageParameters parameters) {
+            add(new SelectionForm("form"));
+        }
+    }
